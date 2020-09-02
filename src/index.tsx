87732f907a42c5e7e5c55caca87c1cc2343fa7d8
saseungmin <dbd02169@naver.com>
2020-09-02 23:23:22 +0900

    update 1회차

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// 인터페이스
+interface AppProps {
+  title: string;
+  color: string;
+}
+
+// 타입 정의
+// 상태 : 시간의 흐름에 따라서 변화하는 것
+// 현재 상태는 불변
+function App(props: AppProps){
+  return (
+    <h1>{props.title}</h1>
+  )
+}
+
+// 버추얼 돔의 랜더를 이용
+// 두개의 인자
+// render최상위에서만 실행
+// 단일 버추얼 돔
 ReactDOM.render(
+  // 첫번째 인자
+  // StrictMode => 경고해주는 안내해주는 개발툴에서 제공하는 툴
   <React.StrictMode>
-    <App />
+    <App title="Tech Hello?" color="blue"/>
   </React.StrictMode>,
+  // 돔으로 변경후 root 컴포넌트에 붙친다.
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
